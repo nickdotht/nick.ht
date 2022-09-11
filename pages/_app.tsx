ef3bb6006b33437a5b4f0b8a9c6bc1d749bc7c8d
Nick Rameau <hello@nick.ht>
2022-09-11 07:26:15 -0400

    Set up Chakra UI and Montserrat font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
+import "@fontsource/montserrat/variable.css"
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    body: "MontserratVariable, sans-serif",
+    heading: "MontserratVariable, sans-serif",
+  },
+
+  styles: {
+    global: {
+      body: {
+        bg: "#12181B",
+      },
+
+      '*:focus': {
+        shadow: 'none !important',
+      },
+    }
+  },
+})
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
