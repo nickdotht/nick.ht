850ec7164fb1cfc327cfbe5e5a4e7c23f4c536e1
Nick Rameau <hello@nick.ht>
2022-09-20 08:27:35 -0400

    WIP: Home page responsiveness

--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Icon, Link, Text } from "@chakra-ui/react"
+import { Box, Flex, Heading, Icon, Link, Text } from "@chakra-ui/react"
 import Image from "next/image"
 import { FC } from "react"
 import { FiGithub, FiLinkedin, FiTwitter } from 'react-icons/fi'
@@ -7,27 +7,46 @@ export const HomeIntro: FC = () => {
   return (
     <Flex
       flex="1"
-      mt="243px"
+      mt={['40px', '40px', '40px', '243px', '243px', '243px']}
     >
       <Flex
         m="auto"
+        direction={['column', 'column', 'column', 'row', 'row', 'row']}
+        px={['16px', '16px', '16px', '40px', '40px', '40px']}
       >
+        <Box display={['flex', 'flex', 'flex', 'none', 'none', 'none']} m="24px auto">
+          <Image src="/images/hero-image.png" width="200px" height="200px" alt="Nick Rameau" />
+        </Box>
+
         {/* Text block */}
         <Flex
           direction="column"
-          mr="218px"
+          mr={['0px', '0px', '0px', '218px', '218px', '218px']}
         >
-          <Heading color="primary" fontSize="52px" fontWeight="700" lineHeight="64px">
+          <Heading
+            color="primary"
+            fontSize="52px"
+            fontWeight="700"
+            lineHeight="64px"
+            textAlign={['center', 'center', 'center', 'left', 'left', 'left']}
+          >
             Joe-Bernick (Nick)<br />Rameau
           </Heading>
 
-          <Text color="#fff" fontWeight="400" fontSize="24px" lineHeight="36px" mt="16px">
+          <Text
+            color="#fff"
+            fontWeight="400"
+            fontSize="24px"
+            lineHeight="36px"
+            mt="16px"
+            textAlign={['center', 'center', 'center', 'left', 'left', 'left']}
+          >
             Based in Haiti, I am a pragmatic, self-taught, and passionate<br />
             software engineer. I currently specialize in modern web and mobile<br />
             technologies, as well as project management.
           </Text>
 
-          <Flex mt="40px">
+          <Flex marginX={['auto', 'auto', 'auto', 0, 0, 0]} mt="40px" mb={['40px', '40px', '40px', 0, 0, 0]}>
             <Link href="https://www.linkedin.com/in/nickdotht/" mr="26px" target="_blank">
               <Icon as={FiLinkedin} color="#fff" _hover={{ color: '#0C63BC' }} boxSize="20px" />
             </Link>
@@ -41,7 +60,9 @@ export const HomeIntro: FC = () => {
         </Flex>
 
         {/* Image block */}
-        <Image src="/images/hero-image.png" width="314px" height="314px" alt="Nick Rameau" />
+        <Box display={['none', 'none', 'none', 'flex', 'flex', 'flex']}>
+          <Image src="/images/hero-image.png" width="270px" height="270px" alt="Nick Rameau" />
+        </Box>
       </Flex>
     </Flex>
   )
